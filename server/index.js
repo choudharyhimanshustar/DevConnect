9387364858db15bb386cdc99cf1560efe657f309
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-12-18 22:57:14 +0530

    Correction in index.js backend

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config();
 const { typeDefs } = require('./schemas/typedefs');
 const { resolvers } = require('./schemas/resolvers');
-const { getUserFromToken } = require('../utils/context');
-const { connectDB } = require('../connection/db');
+const { getUserFromToken } = require('./utils/context');
+const { connectDB } = require('./connection/db');
 
 connectDB();
 
