63f30cc136a864dec5951a2533528ea1a087227d
Choudhary Himanshu <himanshuch3003@gmail.com>
2025-01-11 20:01:46 +0530

    Tailwind configuration updated

--- devconnect/src/ui/Intro.jsx
+++ devconnect/src/ui/Intro.jsx
@@ -19,44 +19,44 @@ const Intro = () => {
                 scrub: true,
             }
         });
-        tl.fromTo("#DarthVader", 
-            { opacity: 0,y:0 }, 
-            { opacity: 1,y:50 });
+        tl.fromTo("#DarthVader",
+            { opacity: 0, y: 100 },
+            { opacity: 1, y: -25 });
     }, [])
     return (
         <div className='w-full h-screen flex  items-center justify-center space-x-10'>
             <div className='flex flex-col w-full max-w-[50vw]  space-y-8'>
-                <div className='space-y-2   flex flex-col text-center mb-5 border border-2 rounded-full p-5 border-dashed'>
+                <div className='space-y-2   flex flex-col text-center mb-5 border border-2 rounded-full  border-dashed hover:rounded-lg'>
                     <PiPlugsConnectedFill className='mx-auto text-3xl' />
                     <span className="font-bold text-xl">Connects Developers</span>
-                    <div className="w-full text-left text-sm max-w-[10vw]">
-                        <span>
-                            Seamless networking, collaboration, and knowledge sharing among developers
-                        </span>
-                    </div>
+
+                    <span>
+                        Seamless networking, collaboration, and knowledge sharing among developers
+                    </span>
+
                 </div>
-                <div className='space-y-4 flex flex-col text-center border border-2 rounded-full p-5 border-dashed'>
+                <div className='space-y-4 flex flex-col text-center border border-2 rounded-full border-dashed'>
                     <BsFillProjectorFill className='mx-auto text-3xl' />
                     <span className="font-bold text-xl">ShowCase Projects</span>
-                    <div className="w-full text-left text-sm max-w-[10vw]">
-                        <span>
-                            Allows developers to display their work, share project details, and demonstrate their skills
-                        </span>
-                    </div>
+
+                    <span>
+                        Allows developers to display their work, share project details, and demonstrate their skills
+                    </span>
+
                 </div>
-                <div className='space-y-4 flex flex-col text-center border border-2 rounded-full p-5 border-dashed'>
+                <div className='space-y-4 flex flex-col text-center border border-2 rounded-full  border-dashed'>
                     <FaHandshake className='mx-auto text-3xl' />
                     <span className="font-bold text-xl">Collaborate</span>
-                    <div className="w-full text-left text-sm max-w-[10vw]">
-                        <span>
+
+                    <span>
                         Enables developers to work together seamlessly, sharing updates, code
-                        </span>
-                    </div>
+                    </span>
+
                 </div>
             </div>
-            <div className='flex flex-col justify-center items-center p-2'  id='DarthVader' >
+            <div className='flex flex-col justify-between items-center p-2'  >
                 <Image src={"https://i.pinimg.com/474x/43/89/2f/43892fabc678e6aad8c76ce73167e3bd.jpg"} width={200} height={100} alt={"StarWars"}
-                    className='fixed bottom'/>
+                     id='DarthVader'/>
                 <span className='font-semibold'>We Would Be Honored If You Would Join Us</span>
             </div>
         </div>
